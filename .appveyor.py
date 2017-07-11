972e2b250e254c846f162d61cb801414c6b116e5
TitanSnow <sweeto@live.cn>
2017-07-11 18:13:28 +0800

    add bdist artifacts

--- .appveyor.py
+++ .appveyor.py
@@ -1,5 +1,6 @@
 from subprocess import check_call
 
 check_call('pip install -r requirements.txt')
+check_call('python setup.py bdist_egg')
 check_call('python setup.py install')
 check_call('python tests.py')
