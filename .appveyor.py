e60563360cb45adfbf4c71d1268cc20b8d703860
TitanSnow <sweeto@live.cn>
2017-07-12 15:32:13 +0800

    build wheel instead of egg

--- .appveyor.py
+++ .appveyor.py
@@ -1,6 +1,6 @@
 from subprocess import check_call, STDOUT
 
 check_call('pip install -r requirements.txt', stderr = STDOUT)
-check_call('python setup.py bdist_egg', stderr = STDOUT)
+check_call('python setup.py bdist_wheel', stderr = STDOUT)
 check_call('python setup.py install', stderr = STDOUT)
 check_call('python tests.py', stderr = STDOUT)
