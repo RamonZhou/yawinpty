fe2f0da0d55ea70c79cf3e47058a22c16f13eaf0
TitanSnow <sweeto@live.cn>
2017-07-12 15:37:29 +0800

    install wheel before build

--- .appveyor.py
+++ .appveyor.py
@@ -1,6 +1,7 @@
 from subprocess import check_call, STDOUT
 
 check_call('pip install -r requirements.txt', stderr = STDOUT)
+check_call('pip install wheel', stderr = STDOUT)
 check_call('python setup.py bdist_wheel', stderr = STDOUT)
 check_call('python setup.py install', stderr = STDOUT)
 check_call('python tests.py', stderr = STDOUT)
