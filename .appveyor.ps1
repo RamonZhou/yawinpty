64532866df096c8d095db4bfe4d12ea5615c40af
TitanSnow <sweeto@live.cn>
2017-07-15 11:41:09 +0800

    build for other python version

--- .appveyor.ps1
+++ .appveyor.ps1
@@ -1,16 +1,26 @@
 $ErrorActionPreference = "Stop"
 Get-ChildItem C:\Python* | ForEach-Object{
     $python = $_.Name
-    if($python.StartsWith('Python35') -or $python.StartsWith('Python36')){
-        Write-Host ('Testing ' + $python) -ForegroundColor Magenta
-        $originPath = $env:Path
-        $env:Path = 'C:\' + $python + '\Scripts;' + 'C:\' + $python + ';' + $env:Path
-        Write-Host (python -c "print(__import__('sys').version)") -ForegroundColor Yellow
+    Write-Host ('Testing ' + $python) -ForegroundColor Magenta
+    $originPath = $env:Path
+    $env:Path = 'C:\' + $python + '\Scripts;' + 'C:\' + $python + ';' + $env:Path
+    Write-Host (python -c "print(__import__('sys').version)") -ForegroundColor Yellow
 
+    if($python.StartsWith('Python35') -or $python.StartsWith('Python36')){
         python .appveyor.py
         if(-not $?){ throw }
-
-        Write-Host ('Success ' + $python) -ForegroundColor Green
-        $env:Path = $originPath
+    }else{
+        $cmd = 'call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" '
+        if($python.EndsWith('x64')){
+            $cmd += 'x64'
+        }else{
+            $cmd += 'x86'
+        }
+        $cmd += ' && python .appveyor.py'
+        cmd /c $cmd
+        if(-not $?){ throw }
     }
+
+    Write-Host ('Success ' + $python) -ForegroundColor Green
+    $env:Path = $originPath
 }
